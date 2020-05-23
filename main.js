9fe66ff2d4839e591071b333c399d010923afa1e
Sakihit0ut <treesidewind@yahoo.co.jp>
2020-05-23 19:25:14 +0900

    コード変更

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
 'use strict';
 window.onload=function(){
-  //コンストラクタ作成
   function Card(suit,num){
     this.suit=suit;
     this.num=num;
+    //以下を追記
+    this.front;
+    this.setFront=function(){
+      this.front=`${this.suit}${this.num<10?'0':''}${this.num}.png`;
+    };
   }
-  //カード配列作成
+
   const cards=[];
   const suits=['s','d','h','c'];
   for(let i=0;i<suits.length;i++){
     for(let j=1;j<=13;j++){
       let card=new Card(suits[i],j);
+      //以下を追加
+      card.setFront();
       cards.push(card);
     }
   }
-  //テーブル作成
   const table=document.getElementById('table');
   for(let i=0;i<suits.length;i++){
     let tr=document.createElement('tr');
     for(let j=0;j<13;j++){
       let td=document.createElement('td');
       let tempCard=cards[i*13+j];
-      td.textContent=`${tempCard.suit}:${tempCard.num}`;
+      //以下を追記
+      td.classList.add('card');
+      td.style.backgroundImage=`url(images/${tempCard.front})`;
+      //以下をコメントアウト(または削除)
+      //td.textContent=`${tempCard.suit}:${tempCard.num}`;
       tr.appendChild(td);
     }
     table.appendChild(tr);
